28141b689e275b08664af6756bed339de8ab5c66
t-dorasani <dorasanammathambichetty@gmail.com>
2026-03-01 20:23:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,8 @@ type StatusResponse = {
   error?: string | null;
 };
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+//const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
 type ImageItem = string | { url: string; label?: string };
 
